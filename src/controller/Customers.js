5cdd0f613eeadbff6773ae21c74ab61f3f612abe
geisniwander <geisniwander@gmail.com>
2023-02-11 22:06:58 -0300

    feat: implementing postCustomers route

--- src/controller/Customers.js
+++ src/controller/Customers.js
@@ -11,8 +11,25 @@ export async function getCustomersById(req, res) {
 }
 
 export async function postCustomers(req, res) {
+  const { name, phone, cpf, birthday } = req.body;
+
   try {
-  } catch {}
+    const customers = await db.query("SELECT cpf FROM customers");
+
+    if (customers.rows.find((customer) => customer.cpf === cpf))
+      return res.send(409);
+
+    const newCustomer = await db.query(
+      `
+      INSERT INTO customers (name,  phone, cpf, birthday)
+      VALUES ($1, $2, $3, $4);`,
+      [name, phone, cpf, birthday]
+    );
+
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
 
 export async function putCustomers(req, res) {
--- src/schema/CustomerSchema.js
+++ src/schema/CustomerSchema.js
@@ -2,7 +2,31 @@ import joi from "joi";
 
 export const customerSchema = joi.object({
   name: joi.string().required(),
-  cpf: joi.number().positive().required(),
+  cpf: joi
+    .string()
+    .custom((value, helper) => {
+      if (isNaN(value)) {
+        return helper.message("CPF must contain only numbers");
+      }
+      if (value.length !== 11) {
+        return helper.message("CPF must be 11 characters long");
+      } else {
+        return true;
+      }
+    })
+    .required(),
   birthday: joi.date().required(),
-  phone: joi.number().positive().required(),
+  phone: joi
+    .string()
+    .custom((value, helper) => {
+      if (isNaN(value)) {
+        return helper.message("Phone must contain only numbers");
+      }
+      if (value.length !== 10 && value.length !== 11) {
+        return helper.message("Phone must be 10 or 11 characters long");
+      } else {
+        return true;
+      }
+    })
+    .required(),
 });
