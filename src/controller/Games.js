738b9d13a1d974fc235d1a1e5b2e484d09603cd0
geisniwander <geisniwander@gmail.com>
2023-02-12 20:16:57 -0300

    feat: implementing query string search in getGames

--- src/controller/Games.js
+++ src/controller/Games.js
@@ -1,8 +1,16 @@
 import { db } from "../config/database.js";
 
 export async function getGames(req, res) {
+  const { name } = req.query;
+
   try {
-    const games = await db.query("SELECT * FROM games");
+    let games = undefined;
+    if (name)
+      games = await db.query(`SELECT * FROM games WHERE upper(name) LIKE $1 `, [
+        name.toUpperCase() + "%",
+      ]);
+    else games = await db.query(`SELECT * FROM games`);
+
     res.status(200).send(games.rows);
   } catch (error) {
     res.status(500).send(error.message);
