e696e29ce57535dc0b167c2abbfb32bb08f89308
geisniwander <geisniwander@gmail.com>
2023-02-11 21:49:17 -0300

    feat: implementing getGames route

--- src/controller/Games.js
+++ src/controller/Games.js
@@ -2,7 +2,11 @@ import { db } from "../config/database.js";
 
 export async function getGames(req, res) {
   try {
-  } catch {}
+    const games = await db.query("SELECT * FROM games");
+    res.status(200).send(games.rows);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
 
 export async function postGames(req, res) {
