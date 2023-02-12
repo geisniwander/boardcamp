f164671f158022813d449ee28a504ad3760a37bd
geisniwander <geisniwander@gmail.com>
2023-02-11 22:08:51 -0300

    feat: implementing getCustomers route

--- src/controller/Customers.js
+++ src/controller/Customers.js
@@ -2,7 +2,11 @@ import { db } from "../config/database.js";
 
 export async function getCustomers(req, res) {
   try {
-  } catch {}
+    const customers = await db.query("SELECT * FROM customers");
+    res.status(200).send(customers.rows);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
 
 export async function getCustomersById(req, res) {
